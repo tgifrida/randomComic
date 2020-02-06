4bcbe0ac5472323fdaf9779b67e868eb2f1e353c
Frida Bredberg <frida.bredberg@telavox.com>
2020-02-06 15:20:58 +0100

    Add spinner

--- src/components/comic/comic.js
+++ src/components/comic/comic.js
@@ -3,32 +3,51 @@ import React from 'react';
 import axios from 'axios';
 import Button from '@material-ui/core/Button'; //https://material-ui.com/
 import './comic.css'
+import { CircularProgress } from '@material-ui/core';
 
 class Comic extends React.Component {
-    state = {
-        index: 654,
-        comicImg: '',
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: 654,
+            comicImg: '',
+            loading: true,
+        }
     }
 
-    getRandomIndex = () => {
+    componentDidMount() {
+        this.makeRequest();
+    }
+
+    getRandomIndex() {
         let randomIndex = Math.floor(Math.random() * 1000);
-        this.makeRequest(randomIndex);
+        this.setState({ index: randomIndex });
     }
 
-    makeRequest = async (randomIndex) => {
-        const url = `http://xkcd.com/${randomIndex}/info.0.json`;
+    makeRequest = async () => {
+        this.setState({loading: true})
+        const url = `http://xkcd.com/${this.state.index}/info.0.json`;
+        
         axios.get(url).then(res => {
             console.log("Result: ", res);
-            this.setState({comicImg: res.data.img});
+            this.setState({ comicImg: res.data.img, loading: false });
         });
+
+    }
+
+    getRandomComic = () => {
+        this.getRandomIndex();
+        this.makeRequest();
     }
 
     render() {
-        console.log(this.state)
         return (
             <div className="container">
-                <Button className="button" variant="contained" color="primary" onClick={this.getRandomIndex}>Random comic</Button>
-                <img src={this.state.comicImg}></img>
+                <Button className="button" variant="contained" color="primary" onClick={this.getRandomComic}>Random comic</Button>
+                <div className="comic-container">
+                    {this.state.loading ? <CircularProgress /> : <img src={this.state.comicImg}></img>}
+                </div>
             </div>
         )
     }
